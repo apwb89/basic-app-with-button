24064db24314abd77ae67bb68ba6c7de3ce1375a
apwb89 <apwb89@gmail.com>
2022-04-29 12:08:25 +0100

    added read/write to collecion

--- App.js
+++ App.js
@@ -1,34 +1,83 @@
 import React, { useState, useEffect } from 'react';
+import { doc, setDoc, updateDoc, collection, getDocs, addDoc } from 'firebase/firestore';
 import {
   StyleSheet,
   Text,
   View,
   TouchableOpacity,
-  Button,
   Alert,
+  LogBox,
+  Button,
+  TextInput
 } from 'react-native';
+import TextInputField from 'react-native'
 
 
-import db from './firebase-config';
+
+LogBox.ignoreLogs(['Setting a timer'])
+
+import  db  from './firebase-config';
 
 function App() {
+  
   const [ test, setTest ] = useState('unpressed')
 
-  const buttonPress = () => {
-    setTest('pressed')
+  const [ name, setName ] = useState('')
+
+  
+  // Read db
+  
+  const colRef = collection(db, 'users')
+  
+  const buttonPress = async () => {
+    (test === 'unpressed') ? setTest('pressed') : setTest('unpressed')
+  
+   
+    
+    getDocs(colRef).then((snapshot) => {
+      let users = []
+      snapshot.docs.forEach((doc) => {
+        users.push({ ...doc.data(), id: doc.id })
+      })
+      console.log(users)
+    }).catch((err) => {
+      console.log(err)
+    })
+    
   }
+    
 
-  useEffect(() => {
 
-  }, [test])
+
+//add to db
+const formInput = () => {
+
+  
+  addDoc(colRef, {
+    name: name
+  }).then(() => {
+    setName('')
+  })
+  
+}
+   
+  
+
+  
+
+
 
   return (
     <View style={styles.setMargin}>
-      <Button title="top-of-app" style={styles.button} onPress={buttonPress}>Button</Button>
+      <Button title="press here to console log users" style={styles.button} onPress={buttonPress}>Button</Button>
       
       <View style={styles.red} onPress={buttonPress}>
       </View>
       <Text style={styles.red}>{test}</Text>
+      <TextInput placeholder="Enter your name here"
+      value={name}
+      onChangeText={text => setName(text)}/>
+      <Button title="Submit" onPress={formInput} />
   
     </View>
   );
